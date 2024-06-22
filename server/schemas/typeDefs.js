7fc19860a3f2491830315eb851c491d20ebd9fcd
Joe <joeyndiebel@gmail.com>
2024-06-22 08:43:10 -0500

    base

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const typeDefs = `
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]!
+  }
+
+  type Book {
+    bookId: ID!
+    description: String!
+    authors: [String]
+    title: String!
+    image: String
+    link: String
+  }
+
+  type Comment {
+    _id: ID
+    commentText: String
+    commentAuthor: String
+    createdAt: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addComment(thoughtId: ID!, commentText: String!): Thought
+    removeThought(thoughtId: ID!): Thought
+    removeComment(thoughtId: ID!, commentId: ID!): Thought
+  }
+`;
+
+module.exports = typeDefs;
