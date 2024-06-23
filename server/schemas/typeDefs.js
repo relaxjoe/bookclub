14a8d31825aa2d81a9f938e616fef8347d78944a
Joe <joeyndiebel@gmail.com>
2024-06-23 08:37:59 -0500

    resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -16,17 +16,18 @@ const typeDefs = `
     link: String
   }
 
-  type Comment {
-    _id: ID
-    commentText: String
-    commentAuthor: String
-    createdAt: String
-  }
-
   type Auth {
     token: ID!
     user: User
   }
+    input BookInput {
+    bookId: ID!
+    description: String!
+    authors: [String]
+    title: String!
+    image: String
+    link: String
+    }
 
   type Query {
     me: User
@@ -35,10 +36,8 @@ const typeDefs = `
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    addThought(thoughtText: String!): Thought
-    addComment(thoughtId: ID!, commentText: String!): Thought
-    removeThought(thoughtId: ID!): Thought
-    removeComment(thoughtId: ID!, commentId: ID!): Thought
+    saveBook(book: BookInput!): User
+    removeBook(BookId: ID!): User
   }
 `;
 
